1a758a0c012d7ce1e8e61d6ff482161a4fb48803
JDWardle <jesse@wardle.dev>
2025-12-11 03:27:35 -0700

    Add command to set other player aggression levels

--- src/main/java/com/jdwardle/passivemobs/CustomCommands.java
+++ src/main/java/com/jdwardle/passivemobs/CustomCommands.java
@@ -9,17 +9,29 @@ import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
 
 import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
 
 public class CustomCommands {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
-        dispatcher.register(Commands.literal("aggression")
-            .executes(CustomCommands::getPlayerAggressionLevel)
+        dispatcher.register(Commands.literal("myaggression")
+            .executes(CustomCommands::getMyAggressionLevel)
             .then(Commands.argument("level", StringArgumentType.string())
                     .suggests(CustomCommands::suggestLevel)
-                    .executes(context -> setPlayerAggressionLevel(context, context.getArgument("level", String.class)))
+                    .executes(context -> setMyAggressionLevel(context, context.getArgument("level", String.class)))
+            )
+        );
+
+        dispatcher.register(Commands.literal("aggression")
+            .then(Commands.argument("player", StringArgumentType.string())
+                .suggests(CustomCommands::suggestPlayers)
+                .executes(context -> getPlayerAggressionLevel(context, context.getArgument("player", String.class)))
+                .then(Commands.argument("level", StringArgumentType.string())
+                    .suggests(CustomCommands::suggestLevel)
+                    .executes(context -> setPlayerAggressionLevel(context, context.getArgument("player", String.class), context.getArgument("level", String.class)))
+                )
             )
         );
     }
@@ -36,7 +48,7 @@ public class CustomCommands {
         return builder.buildFuture();
     }
 
-    public static int setPlayerAggressionLevel(CommandContext<CommandSourceStack> context, String aggressionLevel) throws CommandSyntaxException {
+    public static int setMyAggressionLevel(CommandContext<CommandSourceStack> context, String aggressionLevel) throws CommandSyntaxException {
         CommandSourceStack source = context.getSource();
 
         if (!source.isPlayer()) {
@@ -59,7 +71,7 @@ public class CustomCommands {
         return 1;
     }
 
-    public static int getPlayerAggressionLevel(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
+    public static int getMyAggressionLevel(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
         CommandSourceStack source = context.getSource();
 
         if (!source.isPlayer()) {
@@ -69,6 +81,46 @@ public class CustomCommands {
         PlayerSettings playerSettings = source.getPlayerOrException().getData(PassiveMobs.PLAYER_SETTINGS);
 
         source.sendSuccess(() -> Component.literal("Aggression level: " + playerSettings.getAggressionLevel().toString()), false);
+        return 1;
+    }
+
+    public static int setPlayerAggressionLevel(CommandContext<CommandSourceStack> context, String playerName, String aggressionLevel) throws CommandSyntaxException {
+        CommandSourceStack source = context.getSource();
+
+        Player player = context.getSource().getServer().getPlayerList().getPlayer(playerName);
+        if (player == null) {
+            source.sendFailure(Component.literal("Invalid player name"));
+            return 0;
+        }
+
+        PlayerSettings playerSettings = source.getPlayerOrException().getData(PassiveMobs.PLAYER_SETTINGS);
+
+        AggressionLevel level;
+        try {
+            level = AggressionLevel.getLevel(aggressionLevel);
+        } catch (IllegalStateException e) {
+            source.sendFailure(Component.literal("Invalid aggression level"));
+            return 0;
+        }
+
+        playerSettings.setAggressionLevel(level);
+
+        source.sendSuccess(() -> Component.literal("Aggression level set to " + level.toString()), false);
+        return 1;
+    }
+
+    public static int getPlayerAggressionLevel(CommandContext<CommandSourceStack> context, String playerName) throws CommandSyntaxException {
+        CommandSourceStack source = context.getSource();
+
+        Player player = context.getSource().getServer().getPlayerList().getPlayer(playerName);
+        if (player == null) {
+            source.sendFailure(Component.literal("Invalid player name"));
+            return 0;
+        }
+
+        PlayerSettings playerSettings = player.getData(PassiveMobs.PLAYER_SETTINGS);
+
+        source.sendSuccess(() -> Component.literal("Player aggression level: " + playerSettings.getAggressionLevel().toString()), false);
         return 1;
     }
 }
