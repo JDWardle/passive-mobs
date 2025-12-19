0196941131c55434587cfca23a9dadff69691cb8
JDWardle <jesse@wardle.dev>
2025-12-18 20:05:00 -0700

    Perform null checks when accessing the player manager and updated comments to be more descriptive.

--- src/main/java/com/jdwardle/passivemobs/PlayerManager.java
+++ src/main/java/com/jdwardle/passivemobs/PlayerManager.java
@@ -2,12 +2,17 @@ package com.jdwardle.passivemobs;
 
 import net.minecraft.world.entity.player.Player;
 
-import static com.jdwardle.passivemobs.PassiveMobs.LOGGER;
 import static com.jdwardle.passivemobs.PassiveMobs.PLAYER_SETTINGS;
 
 public class PlayerManager {
+    // This tracks if a player should appear aggressive to mobs or not. The
+    // behavior of when this is set to true is determined by the players
+    // configured aggression level.
     private Boolean aggressive;
+
+    // This tracks the last time the player was set to aggressive.
     private int lastAggressionTimestamp;
+
     private final Player player;
     private final PlayerSettings playerSettings;
 
@@ -30,6 +35,10 @@ public class PlayerManager {
         }
     }
 
+    // This handles the aggression timer. Called on some sort of regular
+    // interval, this will check the player tick count and compare it to their
+    // lastAggressionTimestamp to determine if a player should go from
+    // aggressive to non-aggressive.
     public void tick() {
         if (player.isDeadOrDying()) {
             return;
@@ -44,7 +53,8 @@ public class PlayerManager {
         }
     }
 
-    // Returns true if the player can be targeted by mobs.
+    // Returns true if the player can be targeted by mobs. Based on their
+    // current aggression state and configured aggression level.
     public Boolean canBeTargeted() {
         switch (this.playerSettings.getAggressionLevel()) {
             case AggressionLevel.NORMAL -> {
@@ -61,7 +71,8 @@ public class PlayerManager {
         return false;
     }
 
-    // Returns true if the player can be damaged by mobs.
+    // Returns true if the player can be damaged by mobs. Based on their current
+    // aggression state and configured aggression level.
     public Boolean canBeDamaged() {
         switch (this.playerSettings.getAggressionLevel()) {
             case AggressionLevel.NORMAL, AggressionLevel.PASSIVE -> {
