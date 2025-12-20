45da5525e8cf94c746bc9344592f7b47ba1556ff
JDWardle <jesse@wardle.dev>
2025-12-19 20:57:53 -0700

    Allow the test mod to access the main mod module

--- src/gametest/java/com/jdwardle/passivemobsgametest/PassiveMobsTest.java
+++ src/gametest/java/com/jdwardle/passivemobsgametest/PassiveMobsTest.java
@@ -1,7 +1,13 @@
 package com.jdwardle.passivemobsgametest;
 
+import com.jdwardle.passivemobs.AggressionLevel;
+import com.jdwardle.passivemobs.Config;
+import com.jdwardle.passivemobs.PassiveMobs;
+import com.jdwardle.passivemobs.PlayerSettings;
 import com.mojang.logging.LogUtils;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.world.level.GameType;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
@@ -16,46 +22,27 @@ import net.neoforged.testframework.impl.MutableTestFramework;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
+import java.util.Objects;
+
 @Mod(PassiveMobsTest.MODID)
 public class PassiveMobsTest {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "passivemobsgametest";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = PassiveMobs.VERSION;
     private static final ResourceLocation TEST_FRAMEWORK_ID = ResourceLocation.fromNamespaceAndPath(MODID, "tests");
 
-    // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public PassiveMobsTest(IEventBus modEventBus, ModContainer modContainer) {
-        // Register ourselves for server and other game events we are interested
-        // in.
-        // Note that this is necessary if and only if we want *this* class to
-        // respond directly to events. Do not add this line if there are no
-        // @SubscribeEvent-annotated functions in this class.
-//        NeoForge.EVENT_BUS.register(this);
-
-        setupTestFramework(modEventBus, modContainer);
-
-        LOGGER.info("Passive Mobs Test loaded version {}!", VERSION);
-    }
-
-    private void setupTestFramework(IEventBus modEventBus, ModContainer modContainer) {
-        // The ID of the framework. Used by logging, primarily
-        // Client-side compatibility configuration. This is COMPLETLY optional, but it is recommended for ease of use.
-        // The key used to toggle the tests overlay
-        // The key used to open the Test Manager screen
         MutableTestFramework testFramework = FrameworkConfiguration.builder(TEST_FRAMEWORK_ID) // The ID of the framework. Used by logging, primarily
                 .clientConfiguration(() -> ClientConfiguration.builder() // Client-side compatibility configuration. This is COMPLETLY optional, but it is recommended for ease of use.
                         .toggleOverlayKey(GLFW.GLFW_KEY_J) // The key used to toggle the tests overlay
                         .openManagerKey(GLFW.GLFW_KEY_N) // The key used to open the Test Manager screen
                         .build())
-                .build().create(); // Build and store the InternalTestFramework. We use the "internal" version because we want to access methods not usually exposed, like the init method
+                .build().create();
 
-        // Initialise this framework, using the mod event bus of the currently loading mod, and the container of the currently loading mod.
-        // The container is used for collecting annotations.
-        // This method will collect and register tests, structure templates, group data, and will fire init listeners.
         testFramework.init(modEventBus, modContainer);
+
+        LOGGER.info("Passive Mobs Test loaded version {}!", VERSION);
     }
 
     @EmptyTemplate(floor = true)
@@ -67,12 +54,33 @@ public class PassiveMobsTest {
     )
     static void onPlayerJoinTest(final DynamicTest test) {
         test.onGameTest(helper -> helper.startSequence(() -> helper.makeTickingMockServerPlayerInLevel(GameType.SURVIVAL))
-//                .thenExecute(player -> {
-//                    helper.assertTrue(player.hasData(, "player should have settings");
-//PassiveMobs.PLAYER_SETTINGS)
-//                    PlayerSettings settings = player.getData(PassiveMobs.PLAYER_SETTINGS);
-//                    helper.assertTrue(settings.getAggressionLevel() == AggressionLevel.getLevel(Config.DEFAULT_AGGRESSION_LEVEL.get()), "new player aggression level should be set to the default");
-//                })
+                .thenExecute(player -> {
+                    helper.assertTrue(player.hasData(PassiveMobs.PLAYER_SETTINGS), "player should have settings");
+
+                    PlayerSettings settings = player.getData(PassiveMobs.PLAYER_SETTINGS);
+                    helper.assertTrue(settings.getAggressionLevel() == AggressionLevel.getLevel(Config.DEFAULT_AGGRESSION_LEVEL.get()), "new player aggression level should be set to the default");
+                    helper.assertTrue(Objects.equals(settings.getVersion(), PlayerSettings.CURRENT_VERSION), "new player settings data version should match the default");
+                })
+                .thenSucceed());
+    }
+
+    @EmptyTemplate(floor = true)
+    @GameTest(template = "empty")
+    @TestHolder(
+            description = "Ensure that mobs target players on normal difficulty",
+            title = "normal_mob_targeting",
+            enabledByDefault = true
+    )
+    static void playerCommandsTest(final DynamicTest test) {
+        test.onGameTest(helper -> helper.startSequence(() -> helper.makeTickingMockServerPlayerInLevel(GameType.SURVIVAL))
+                .thenExecute(player -> {
+                    PlayerSettings settings = player.getData(PassiveMobs.PLAYER_SETTINGS);
+                    settings.setAggressionLevel(AggressionLevel.NORMAL);
+
+                    Zombie entity = helper.spawn(EntityType.ZOMBIE, 0, 0, 5);
+//                    entity.getTarget()
+                    // tap into target entity event somehow.
+                })
                 .thenSucceed());
     }
 }
