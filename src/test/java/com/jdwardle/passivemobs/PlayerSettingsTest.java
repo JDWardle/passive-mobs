77f4ec8050643cd2ea35a68e9bd33620d300f87c
JDWardle <jesse@wardle.dev>
2025-12-19 17:04:11 -0700

    Working mod specifically for game tests

---
+++ src/test/java/com/jdwardle/passivemobs/PlayerSettingsTest.java
@@ -0,0 +1,28 @@
+package com.jdwardle.passivemobs;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerSettingsTest {
+
+    @Test
+    void getAggressionLevel() {
+    }
+
+    @Test
+    void setAggressionLevel() {
+    }
+
+    @Test
+    void getVersion() {
+    }
+
+    @Test
+    void serialize() {
+    }
+
+    @Test
+    void deserialize() {
+    }
+}
